ffbfe5377ac29b2c32de0aa581d95491b85999e2
Ahmed090900 <ahmed1411tarek97@gmail.com>
2026-03-07 22:19:54 +0400

    Add payment completion handler in complete.js

---
+++ api/complete.js
@@ -0,0 +1,19 @@
+export default async function handler(req,res){
+
+const {paymentId}=req.body;
+
+const response=await fetch(
+`https://api.minepi.com/v2/payments/${paymentId}/complete`,
+{
+method:"POST",
+headers:{
+Authorization:`Key ${process.env.PI_API_KEY}`
+}
+}
+);
+
+const data=await response.json();
+
+res.status(200).json(data);
+
+}
