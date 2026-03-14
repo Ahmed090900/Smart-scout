9e7a669983cfa4f57f429ea6f855ab7b320298a8
Ahmed090900 <ahmed1411tarek97@gmail.com>
2026-03-14 20:49:31 +0400

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -4,20 +4,32 @@ if (req.method !== "POST") {
 return res.status(405).json({error:"Method not allowed"});
 }
 
+try {
+
 const { paymentId } = req.body;
 
 const response = await fetch(
-"https://api.minepi.com/v2/payments/" + paymentId + "/approve",
+`https://api.minepi.com/v2/payments/${paymentId}/approve`,
 {
 method:"POST",
 headers:{
-Authorization:"Key " + process.env.PI_API_KEY
+Authorization:`Key ${process.env.PI_API_KEY}`,
+"Content-Type":"application/json"
 }
 }
-)
+);
 
 const data = await response.json();
 
 res.status(200).json(data);
+
+} catch(e){
+
+res.status(500).json({
+error:"approve failed",
+details:e.message
+});
+
+}
 
 }
