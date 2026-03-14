23d5df9128b644d7ce17583bbb950c3587fbf152
Ahmed090900 <ahmed1411tarek97@gmail.com>
2026-03-14 10:27:33 +0400

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,37 +1,21 @@
-export default async function handler(req, res) {
+export default async function handler(req,res){
 
-if (req.method !== "POST") {
-return res.status(405).json({ error: "Method not allowed" });
-}
+const apiKey = process.env.PI_API_KEY
 
-const paymentId = req.body.paymentId;
-
-const apiKey = process.env.PI_API_KEY;
-
-try {
+const {paymentId} = req.body
 
 const response = await fetch(
 `https://api.minepi.com/v2/payments/${paymentId}/complete`,
 {
-method: "POST",
-headers: {
-Authorization: `Key ${apiKey}`,
-"Content-Type": "application/json"
+method:"POST",
+headers:{
+Authorization:`Key ${apiKey}`,
+"Content-Type":"application/json"
 }
-}
-);
+})
 
-const data = await response.json();
+const data = await response.json()
 
-return res.status(200).json(data);
-
-} catch (error) {
-
-return res.status(500).json({
-error: "Server error",
-details: error.message
-});
-
-}
+res.status(200).json(data)
 
 }
