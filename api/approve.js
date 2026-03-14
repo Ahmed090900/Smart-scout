1ee2ceb29897a36bd167af3a2af5d60d6c958921
Ahmed090900 <ahmed1411tarek97@gmail.com>
2026-03-14 20:28:16 +0400

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,38 +1,23 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
 
-try{
-
-if(req.method !== "POST"){
-return res.status(405).json({error:"POST only"})
+if (req.method !== "POST") {
+return res.status(405).json({error:"Method not allowed"});
 }
-
-const apiKey = process.env.PI_API_KEY;
 
 const { paymentId } = req.body;
 
 const response = await fetch(
-`https://api.minepi.com/v2/payments/${paymentId}/approve`,
+"https://api.minepi.com/v2/payments/" + paymentId + "/approve",
 {
 method:"POST",
 headers:{
-Authorization:`Key ${apiKey}`,
-"Content-Type":"application/json"
+Authorization:"Key " + process.env.PI_API_KEY
 }
 }
-);
+)
 
 const data = await response.json();
 
 res.status(200).json(data);
-
-}catch(e){
-
-res.status(500).json({
-error:e.message
-});
-
-}
 
 }
