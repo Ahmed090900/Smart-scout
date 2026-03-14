f2f1d18cf25c39e41a3c0cc68ff08b7869e85d1e
Ahmed090900 <ahmed1411tarek97@gmail.com>
2026-03-14 10:27:08 +0400

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,24 +1,8 @@
-export default async function handler(req, res) {
-
-try {
-
-console.log("BODY:", req.body)
-
-if(req.method !== "POST"){
-return res.status(405).json({message:"POST only"})
-}
+export default async function handler(req,res){
 
 const apiKey = process.env.PI_API_KEY
 
-if(!apiKey){
-return res.status(500).json({error:"API KEY NOT FOUND"})
-}
-
-const { paymentId } = req.body || {}
-
-if(!paymentId){
-return res.status(400).json({error:"paymentId missing"})
-}
+const {paymentId} = req.body
 
 const response = await fetch(
 `https://api.minepi.com/v2/payments/${paymentId}/approve`,
@@ -28,22 +12,10 @@ headers:{
 Authorization:`Key ${apiKey}`,
 "Content-Type":"application/json"
 }
-}
-)
+})
 
 const data = await response.json()
 
-return res.status(200).json(data)
-
-}catch(error){
-
-console.error("SERVER ERROR:",error)
-
-return res.status(500).json({
-error:error.message,
-stack:error.stack
-})
-
-}
+res.status(200).json(data)
 
 }
